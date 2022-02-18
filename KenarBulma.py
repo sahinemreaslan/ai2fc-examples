85aa17f6365161e22be74f95eb8322ea41c4074a
VoidString <43322788+voidstring@users.noreply.github.com>
2022-02-18 21:10:26 +0300

    Update KenarBulma.py

--- KenarBulma.py
+++ KenarBulma.py
@@ -1,35 +1,41 @@
-#Kenar Bulma Algoritmalarının Uygulanması - RobertCross Aİ2FC TEAM App
 import numpy as np
 from PIL import Image
 #Author = @voidstring
 imageAdrr = "C:/Users/Emre/Desktop/Deneme.jpg"
-myİmage = Image.open(imageAdrr)
-myİmage = np.array(myİmage)
+myİmageOne = Image.open(imageAdrr)
+myİmage = np.array(myİmageOne)
 
 robertCrossX = np.array([[1,0],[0,-1]])
 robertCrossy = np.array([[0,1],[-1,0]])
 
 
-def Compute(i, j):
-    RenkR = int(myİmage[i][j][0])
-    RenkG = int(myİmage[i][j][1])
-    RenkB = int(myİmage[i][j][2])
-    P = (RenkR + RenkG + RenkB)/3
+#numpy nedir ? Matris üzerinde işlem yapmamızı KOLAYLAŞTIRAN kütüphanedir.
+
+def Compute_P(i, j):
+    RenkR = int(myİmage[i][j][0])#8 BİT 0-255
+    RenkG = int(myİmage[i][j][1])#8 BİT 0-255
+    RenkB = int(myİmage[i][j][2])#8 BİT 0-255
+    #+
+    #------------------------------------------
+    #24 BİT
+    P = (RenkR + RenkG + RenkB)/3 # 8 BİT 0-255
     return P
 
 
 for i in range(0,myİmage.shape[0]-1):
     for j in range(0,myİmage.shape[1]-1):
             #x,y
-        p1 = Compute(i,j)
-        p2 = Compute(i+1,j)
-        p3 = Compute(i,j+1)
-        p4 = Compute(i+1,j+1)
+        p1 = Compute_P(i,j)#(x,y)
+        p2 = Compute_P(i+1,j)#(x+1,y)
+        p3 = Compute_P(i,j+1)#(x,y+1)
+        p4 = Compute_P(i+1,j+1)##(x+1,y+1)
         x = abs(p1-p4)
         y = abs(p2-p3)
         robertCrossVal = x + y
+        
+        
         if robertCrossVal > 255: robertCrossVal = 255
-        myİmage[i][j] = np.array([robertCrossVal,robertCrossVal,robertCrossVal])
+        myİmage[i][j] = np.array([robertCrossVal])
             
 resultMyİmage = Image.fromarray(myİmage)
-resultMyİmage.save('resultMyİmage.png')
+resultMyİmage.save('resultMyİmage2.png')
